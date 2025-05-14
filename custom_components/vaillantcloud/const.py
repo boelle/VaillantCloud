9463b57ad3d366d57932c6d6f18e2c43225bbd7d
rmalbrecht <49707000+rmalbrecht@users.noreply.github.com>
2025-05-14 09:27:19 +0200

    Update const.py

--- custom_components/vaillantcloud/const.py
+++ custom_components/vaillantcloud/const.py
@@ -1,6 +1,6 @@
 from myPyllant.enums import ZoneOperatingType
 
-DOMAIN = "mypyllant"
+DOMAIN = "vaillantcloud"
 OPTION_UPDATE_INTERVAL = "update_interval"
 OPTION_UPDATE_INTERVAL_DAILY = "update_interval_daily"
 OPTION_REFRESH_DELAY = "refresh_delay"
